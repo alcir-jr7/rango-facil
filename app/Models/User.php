df247c300796dc8168cf02139e570742cedcd4a7
Ranieri Valença <ranierivalenca@gmail.com>
2025-10-28 10:23:38 -0300

    feat(#2): add phone number attributes to User model for mass assignment and casting

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone_number',
+        'phone_verified_at',
         'password',
     ];
 
@@ -43,6 +45,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'phone_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
