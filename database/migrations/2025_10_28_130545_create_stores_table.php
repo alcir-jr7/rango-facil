70255fb2e008962526d45aa0e64bc63857d0fc4d
Ranieri Valença <ranierivalenca@gmail.com>
2025-12-16 09:47:23 -0300

    fix: Removendo coluna adicionada em uma migration passada

--- database/migrations/2025_10_28_130545_create_stores_table.php
+++ database/migrations/2025_10_28_130545_create_stores_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image')->nullable();
             $table->boolean('is_open')->default(false);
             $table->boolean('auto_confirm_orders')->default(false);
             $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
