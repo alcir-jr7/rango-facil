b0bd49fbb42b61ded47e00ceda568eb7b634efc3
Yara Regina <yrds@discente.ifpe.edu.br>
2026-01-06 10:19:11 -0300

    feat(#34): Removidas rotas de API: atualiza app.php e exclui api.php

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StoreController;
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/stores/statuses', [StoreController::class, 'statuses']);
-    Route::post('/stores/{store}/toggle-status', [StoreController::class, 'toggleStatus']);
-});
